354a3f2e64d2981ca84b2f77d8ae871c061bd81f
kris <kris@digitalarm.co.uk>
2018-04-09 18:56:48 +0100

    Init

---
+++ app/Models/Thread.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+
+    public function path() {
+    	return '/threads/' . $this->id;
+    }
+}
